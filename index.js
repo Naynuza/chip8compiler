7738a781293d2029b705df6a8df2554a41bc8ec1
Naynuza <naynuza19@gmail.com>
2019-12-05 02:06:46 +0000

    Change lexer rule order

--- index.js
+++ index.js
@@ -12,6 +12,18 @@ let output = document.getElementById("output");
 
 let lexer = new Tokenizr();
 
+lexer.rule(/clear/, (ctx, match) => {
+    ctx.accept("keyword");
+});
+lexer.rule(/call/, (ctx, match) => {
+    ctx.accept("keyword");
+});
+lexer.rule(/return/, (ctx, match) => {
+    ctx.accept("keyword");
+});
+lexer.rule(/skip/, (ctx, match) => {
+    ctx.accept("keyword");
+});
 lexer.rule(/\w+/, (ctx, match) => {
     ctx.accept("identifier");
 });
@@ -50,18 +62,6 @@ lexer.rule(/\=/, (ctx, match) => {
 });
 lexer.rule(/\!/, (ctx, match) => {
     ctx.accept("operator");
-});
-lexer.rule(/clear/, (ctx, match) => {
-    ctx.accept("keyword");
-});
-lexer.rule(/call/, (ctx, match) => {
-    ctx.accept("keyword");
-});
-lexer.rule(/return/, (ctx, match) => {
-    ctx.accept("keyword");
-});
-lexer.rule(/skip/, (ctx, match) => {
-    ctx.accept("keyword");
 });
 lexer.rule(/\{/, (ctx, match) => {
     ctx.accept("separator");
